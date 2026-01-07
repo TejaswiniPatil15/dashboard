4b4bed43a8f8cdad57a95eaacb2ca0e54b427e60
tejaswini15patil@gmail.com <tejaswini15patil@gmail.com>
2026-01-07 22:08:47 +0530

    remove unused TagModule import and update global filter field to 'firstName'

--- src/app/components/product.component/product.component.ts
+++ src/app/components/product.component/product.component.ts
@@ -2,14 +2,13 @@ import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TableModule } from 'primeng/table';
-import { TagModule } from 'primeng/tag';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-product.component',
   standalone: true,
-  imports: [CommonModule, TableModule, TagModule, ButtonModule, InputTextModule],
+  imports: [CommonModule, TableModule, ButtonModule, InputTextModule],
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css'],
 })
